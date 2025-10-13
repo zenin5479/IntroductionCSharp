ddc1510ee0cedd3897cffd7cfd95055c74485ae0
zenin5479 <zenin5479@gmail.com>
2025-10-13 08:58:49 +0300

    Update 14.6 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -208,12 +208,22 @@ namespace IntroductionCSharp
             }
             else
             {
-               for (int i = -1; i >= n; i--)
+               int i = -1;
+               while (i >= n)
                {
+                  
                   if (i % 2 == 0)
                   {
                      Console.Write("{0} ", +i);
                   }
+                  
+                  i--;
+               }
+
+
+               for ( )
+               {
+                  
                }
             }
          }
