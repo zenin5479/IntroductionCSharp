93a3f5f80e9250376933ed12b5bfde98a60a12f2
zenin5479 <zenin5479@gmail.com>
2025-10-13 08:45:25 +0300

    Update 13.4 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -85,7 +85,7 @@ namespace IntroductionCSharp
          Console.WriteLine("Максимальное число: " + max);
 
          Console.WriteLine("--------------------------------------------------");
-         Console.WriteLine("Определение четности числа");
+         Console.WriteLine("Определение четности целого числа");
          Console.WriteLine("--------------------------------------------------");
          Console.Write("Введите целое число: ");
          string stroka = Console.ReadLine();
